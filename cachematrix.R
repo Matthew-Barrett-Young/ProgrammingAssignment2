c6a9b5fc48cd416ba09cb18eb72f8993a73a884e
Matthew-Barrett-Young <68834339+Matthew-Barrett-Young@users.noreply.github.com>
2021-02-20 10:48:46 -0600

    First Edits
    This first commit is mainly to upload my code so I can work on what I have on another computer.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,37 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        cashed <- x
+        set_cashed <- function(){
+                cashed <<- solve(x)
+        }
+        get_cashed <- function(){
+                cashed
+        }
+        list(set_cashed = set_cashed, get_cashed = get_cashed)
 
 }
 
+z <- makeCacheMatrix(A)
+z$get_cashed()
 
+t <- cacheSolve(A, z)
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if(A = z$get_cashed){
+                
+        }
 }
+makeCacheMatrix(23)
+
+solve()
+?det
+A <- matrix( c(5, 1, 0,
+               3,-1, 2,
+               4, 0,-1), nrow=3, byrow=TRUE)
+B <- solve(A)
+C <- solve(B)
+
+
