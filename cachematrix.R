95076b36db81f421e6c572909320ba26064bf852
Matthew <mattbarrett500@gmail.com>
2021-02-21 15:18:03 -0600

    Finished code for project

--- cachematrix.R
+++ cachematrix.R
@@ -1,71 +1,72 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function takes an input, x, that is a matrix. The function then
+##      creates a null variable to later store the inverse and creates four
+##      functions to be used later in cacheSolve.
 
 makeCacheMatrix <- function(x = matrix()) {
         inverse_cached <- NULL                 #Initializes inverse variable         
-                                               
+        
         set_matrix <- function(new_matrix){    #This function assigns the new 
-                                               # matrix to parent environment
-                                               # and wipes the old inverse
+                # matrix to parent environment
+                # and wipes the old inverse
                 inverse_cached <<- NULL
                 x <<- new_matrix
         }
-        get_cached <- function(){              #This function returns cached
+        get_cached <- function(){              #This function returns inverse_cached
                 return(inverse_cached)
         }
-        set_inverse <- function(solved_matrix){#This function sets inverse to 
-                                               # inverse_cached
+        set_inverse <- function(solved_matrix){#This function sets the inverse of 
+                # to x to inverse_cached
                 inverse_cached <<- solved_matrix
         }              
         get_matrix <- function() {             #This function returns the 
-                                               # original matrix
+                # original matrix
                 return(x)
         }
         list(get_matrix = get_matrix,
              set_inverse = set_inverse,
              set_matrix = set_matrix,          
              get_cached = get_cached)
-
+        
 }
-z
-z <- makeCacheMatrix(A)
-
-z$get_cached()
-z$set_matrix(A)
-z$get_matrix()
-B <- z$set_inverse(solve(A))
-## Write a short comment describing this function
+## This function utilizes the environment and list from makeCacheMatrix
+##      that is passed in an object. The function then either solves and
+##      returns the inverse or pulls the inverse that was cached already.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        print(x$inverse_cached)
-        if(!is.null(x$inverse_cached)) {
+        inverse <- x$get_cached()              #Pull the cached inverse
+        
+        if(!is.null(inverse)) {                #Checks if inverse has already been solved                
                 print("Retrieving cached inverse.")
-                return(x$inverse_cached)
+                return(inverse)
         }
-        else {
+        else {                                 #If the inverse hasn't been solved then
+                # the function solves for the inverse and
+                # stores it in the cache
                 print("Calculating inverse of matrix.")
                 original_matrix <- x$get_matrix()
                 solved_matrix <- solve(original_matrix)
                 x$set_inverse(solved_matrix)
-                print(x$inverse_cached)
                 
                 return(solved_matrix)
         }
 }
-cacheSolve(z)
-z$get_cached()
-z$inverse_cached
-makeCacheMatrix(23)
 
-solve()
-?det
 A <- matrix( c(5, 1, 0,
                3,-1, 2,
                4, 0,-1), nrow=3, byrow=TRUE)
-B <- solve(A)
-C <- solve(B)
+z <- makeCacheMatrix(A)
+cacheSolve(z)
 
 
+D <- matrix( c(8, 6, 8, 4, 3,
+               4, 5, 3, 2, 1,
+               4, 3, 2, 1, 4,
+               4, 6, 4, 3, 2,
+               4, 11, 22, 434, 32), nrow = 5, byrow = TRUE)
+
+final <- makeCacheMatrix(D)
+cacheSolve(final)
